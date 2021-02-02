63d7f2720b2ff491bc9b482c6fdf2b4af1d42e6a
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-02 15:19:55 +0100

    fetch top artists

--- modules/connectSpotify.js
+++ modules/connectSpotify.js
@@ -28,49 +28,17 @@ const scopes = [
 ];
 
 console.log(_token);
-// If there is no token, redirect to Spotify authorization
+
 connectToSpotify.addEventListener('click', () => {
   if (!_token) {
     window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
   }
 })
-/**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
- */
 
-// var request = require(['request'], () => console.log('loaded')); // "Request" library
-//
-// // your application requests authorization
-// var authOptions = {
-//   url: 'https://accounts.spotify.com/api/token',
-//   headers: {
-//     'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
-//   },
-//   form: {
-//     grant_type: 'client_credentials'
-//   },
-//   json: true
-// };
-//
-// request.post(authOptions, function(error, response, body) {
-//   if (!error && response.statusCode === 200) {
-//
-//     // use the access token to access the Spotify Web API
-//     var token = body.access_token;
-//     var options = {
-//       url: 'https://api.spotify.com/v1/me/',
-//       headers: {
-//         'Authorization': 'Bearer ' + token
-//       },
-//       json: true
-//     };
-//     request.get(options, function(error, response, body) {
-//       console.log(body);
-//     });
-//   }
-// });
+if (_token) {
+  fetch('https://api.spotify.com/v1/me/top/artists').then(response => {
+    return response.json()
+  }).then(data => {
+    console.log(data);
+  })
+}
