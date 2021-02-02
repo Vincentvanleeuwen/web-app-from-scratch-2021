265324ab47a57b83685922e172de91a963b3f9d0
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-02 14:45:24 +0100

    deleted node, express etc

---
+++ environment.js
@@ -0,0 +1,1 @@
+export const clientID = 'b364d3b9da734592b09d1f0ae8ea7fd5';
---
+++ modules/connectSpotify.js
@@ -0,0 +1,36 @@
+import { clientID } from '../environment.js'
+
+// https://gist.github.com/arirawr/f08a1e17db3a1f65ada2c17592757049
+
+// Get the hash of the url
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+console.log(hash)
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const redirectUri = 'compare-music.netlify.com';
+const scopes = [
+  'user-read-email',
+  'user-read-private',
+  'user-library-read'
+];
+
+// If there is no token, redirect to Spotify authorization
+document.getElementById('login-button').addEventListener('click', () => {
+  if (!_token) {
+    window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
+  }
+})
