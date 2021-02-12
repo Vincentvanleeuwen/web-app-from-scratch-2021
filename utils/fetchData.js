4d94b79bd56299d20bf9926590ee9e3f42081907
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-12 16:07:46 +0100

    responsive changes

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,4 +1,4 @@
-// Fetch the database URL
+// Fetch the database URLs
 export const fetchData = (endPoints, options) => {
   const allEndpoints = endPoints.map(endPoint => fetch(endPoint, options));
   return Promise.all(allEndpoints);
