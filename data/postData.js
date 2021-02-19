cf291d1ed199bd9d0eec0c58da3b05074c26d115
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-19 13:16:40 +0100

    changed postdata

--- data/postData.js
+++ data/postData.js
@@ -2,30 +2,33 @@ import { fetchData } from "../utils/fetchData.js";
 import { convertToJSON } from "../utils/convertToJSON.js";
 import { hashToken } from "../modules/spotifyAuth.js";
 
-let options;
-if( hashToken) {
-  const playlistObject = JSON.parse(window.localStorage.getItem('playlist'));
 
-  options = {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer ' + hashToken,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "name": playlistObject.playlistName,
-      "description": "Made with Combinify",
-      "public": true
-    }),
-    json: true
-  }
-}
 
 
 
 let isLoading = true;
 // Export the data so routes can use it.
 export const postData = (id) => {
+  let options;
+
+  if(hashToken) {
+    const playlistObject = JSON.parse(window.localStorage.getItem('playlist'));
+
+    options = {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + hashToken,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "name": playlistObject.playlistName,
+        "description": "Made with Combinify",
+        "public": true
+      }),
+      json: true
+    }
+  }
+  
   console.log(id);
   const endPoints = [
     `https://api.spotify.com/v1/users/${id}/playlists`,
