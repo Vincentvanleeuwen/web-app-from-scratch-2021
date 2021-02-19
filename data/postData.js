8f51bb353d02f78e3d72e495b8ae098207b3d60f
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-19 13:14:00 +0100

    more safety

--- data/postData.js
+++ data/postData.js
@@ -2,24 +2,27 @@ import { fetchData } from "../utils/fetchData.js";
 import { convertToJSON } from "../utils/convertToJSON.js";
 import { hashToken } from "../modules/spotifyAuth.js";
 
+let options;
 if( hashToken) {
   const playlistObject = JSON.parse(window.localStorage.getItem('playlist'));
+
+  options = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + hashToken,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "name": playlistObject.playlistName,
+      "description": "Made with Combinify",
+      "public": true
+    }),
+    json: true
+  }
 }
 
 
-const options = {
-  method: 'POST',
-  headers: {
-    'Authorization': 'Bearer ' + hashToken,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    "name": playlistObject.playlistName,
-    "description": "Made with Combinify",
-    "public": true
-  }),
-  json: true
-}
+
 let isLoading = true;
 // Export the data so routes can use it.
 export const postData = (id) => {
