cad0d4da179d5d5a406f76f1042fc83a78fed374
Vincent van Leeuwen <shoockz@hotmail.com>
2021-02-28 16:03:04 +0100

    Changes to requests

--- views/playlist.js
+++ views/playlist.js
@@ -30,14 +30,14 @@ export const playlistView = (getProfile, getSongs) => {
    postData(data[0].id)
    .then(playlist => {
      console.log('playlist', playlist)
-     // window.localStorage.setItem('playlistID', playlist.id)
-     // window.localStorage.setItem('playlistHref', playlist.href)
 
      copyBtn.addEventListener('click', () => {
-       copyTxt.value = playlist.href
-       console.log(copyTxt.value)
+       copyTxt.value = playlist.external_urls.spotify
+       // Select the url
        copyTxt.select();
+       // Enable copy on mobile devices
        copyTxt.setSelectionRange(0, 99999);
+       // Copy text
        document.execCommand('copy')
        copyBtn.innerHTML = "Copied URL"
      })
@@ -56,38 +56,44 @@ export const playlistView = (getProfile, getSongs) => {
    else {
      loginScreen.classList.remove('show-screen-flex')
 
-     let people = data;
-      console.log(data)
-     let directives = {
-       image: {
-         src: function(params) {
-           return this.img;
+     getSongs().then(songs => {
+       data[0].songs = songs
+       let people = data;
+       console.log(data)
+       let directives = {
+         image: {
+           src: function(params) {
+             return this.img;
+           },
+           alt: function(params) {
+             return this.name + 'profile picture';
+           },
          },
-         alt: function(params) {
-           return this.name + 'profile picture';
+         playlistname: {
+           text: function(params) {
+             return playlistObject.playlistName
+           }
          },
-       },
-       playlistname: {
-         text: function(params) {
-           return playlistObject.playlistName
-         }
-       },
-       songs: {
-         html: function(params) {
-           getSongs().then(songs => {
-             data[0].songs = songs
-             songs.map(song => {
+         songs: {
+           html: function(params) {
+             // getSongs().then(songs => {
+             //   data[0].songs = songs
+             console.log(this)
+             this.songs.map(song => {
                return `<li>
                <span> ${song.songArtist}</span>     
                <span> ${song.songName}</span>     
               </li>`
              }).join('')
-           })
+             // })
+           }
          }
-       }
-     };
+       };
 
-     Transparency.render(userProfile, people, directives, {debug: true})
+       Transparency.render(userProfile, people, directives, {debug: true})
+     }).catch(err => console.warn('err'))
+
+
    }
  }).catch(err => console.log(err))
 
