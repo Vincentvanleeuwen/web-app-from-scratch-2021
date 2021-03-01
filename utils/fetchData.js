1694037ae528f581c06146e1561eeddc20fc65ee
Vincent van Leeuwen <shoockz@hotmail.com>
2021-03-01 19:37:19 +0100

    debugging

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,6 @@
 // Fetch the database URLs
 export async function fetchData(endPoint, options) {
+  console.log('endPoint =', endPoint);
   const response = await fetch(endPoint, options);
   return await response.json()
 }
